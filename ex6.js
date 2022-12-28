dd4317c89c25d0b9ac03b1016348fe03856a115c
anhduy2512033 <118426265+anhduy2512033@users.noreply.github.com>
2022-12-28 10:18:12 +0700

    Create ex6.js

---
+++ ex6.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+    init();
+    alert("Hello");
+}
+function init(){
+    document.getElementById("magic").onmouseover = function(){
+        this.className = "highlight";
+    }
+    document.getElementById("magic").onmouseout = function(){
+        this.className = "";
+    }    
+}
